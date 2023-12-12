d24152342188c8b6d3ac3b700beb2f9186f74086
Pankaj Koirala <bidhyasapkota@Budi-Mac.local>
2023-12-11 22:08:40 -0700

    string extensions

--- lib/src/extensions/string_extension.dart
+++ lib/src/extensions/string_extension.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import '../simple_reg_exp.dart';
 
 /// Useful extension functions for [String]
@@ -5,9 +7,6 @@ extension SimpleUtilsStringX on String {
   /// Returns the capitalized string
   String get capitalize =>
       '${this[0].toUpperCase()}${substring(1).toLowerCase()}';
-
-  ///
-  String get validatedURL => startsWith('http') ? this : 'https://$this';
 
   /// Convert string date to local date time
   DateTime? get toLocalDateTime {
@@ -31,15 +30,51 @@ extension SimpleUtilsStringX on String {
   /// Phone validation
   bool get isValidPhone => SimpleRegExp.phone.hasMatch(this);
 
+  /// Returns true if all characters in the string are in lower case.
+  bool get isLower => toLowerCase() == this;
+
+  /// Returns true if all characters in the string are in upper case.
+  bool get isUpper => toUpperCase() == this;
+
+  /// Returns true if the string consists of the same character, case insensitive.
+  bool get containsSameChar {
+    if (isEmpty) return false;
+    final char = this[0].toLowerCase();
+    for (var i = 1; i < length; i++) {
+      if (this[i].toLowerCase() != char) return false;
+    }
+    return true;
+  }
+
+  /// Converts string to lowerCase/upperCase based on the first character (base).
+  /// If the first character is not a letter, the original string is returned.
+  String get casedFromBase {
+    if (isEmpty) return '';
+    return this[0].isLower ? toLowerCase() : toUpperCase();
+  }
+
   ///
-  bool? get boolOrNull {
+  bool get toBool {
     switch (toLowerCase()) {
-      case 'true':
+      case 'true' || '1':
         return true;
-      case 'false':
+      case 'false' || '0':
         return false;
       default:
         return false;
     }
+  }
+
+  /// Convert string into the year
+  int get toYear {
+    final input = int.tryParse(this) ?? 0;
+    if (input == 0) return 0;
+    final currentYear = DateTime.now().year;
+    if ('$currentYear'.length - length > 0) {
+      final factor = math.pow(10, length);
+      final prefix = (currentYear ~/ factor) * factor;
+      return (prefix + input).toInt();
+    }
+    return input;
   }
 }
